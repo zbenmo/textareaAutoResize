f955c30bd0e4c175caca77b291710ae7dc819fe1
Oren ZBM <zbenmo@gmail.com>
2012-11-08 14:22:15 +0000

    replaced tabs with spaces

--- textareaAutoResize.js
+++ textareaAutoResize.js
@@ -1,21 +1,20 @@
-(function( $ ) {
-	
-	function resize() {
-		this.height('1px');
-		var newHeight = 5+this.prop('scrollHeight');
-		if (newHeight < 15) newHeight = 15;
-		this.height(newHeight + 'px');
-	}
-	
-  $.fn.textAreaAutoResize = function() {
-  	
-  	this.on('input', function() {
-  		var $this = $(this);
-  		var r = resize.bind($this);
-  		r();
-  	});
-    
-    this.resize();
+(function($) {
+
+  function resize() {
+    this.height('1px');
+    var newHeight = 5+this.prop('scrollHeight');
+    if (newHeight < 15) newHeight = 15;
+    this.height(newHeight + 'px');
+  }
+
+  $.fn.textareaAutoResize = function() {
+
+   this.on('input', function() {
+      var $this = $(this);
+      resize.bind($this)();
+   });
+
+   resize.bind(this)();
   };
-  
-})( jQuery );
+
+})(jQuery);
